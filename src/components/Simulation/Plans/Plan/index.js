3f297c16c7203c6a79f91975cc529f659e04430a
samuel.moura1 <samuel.moura@izap.com.br>
2020-12-23 00:17:35 -0300

    fix: small fix at plan click logic

--- src/components/Simulation/Plans/Plan/index.js
+++ src/components/Simulation/Plans/Plan/index.js
@@ -5,12 +5,22 @@ import { Container, Content, Title, MinutesFree } from "./styles";
 const Plan = ({ plan }) => {
   const { currentPlan, setCurrentPlan } = usePlanContext();
 
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    if (currentPlan) {
+      setCurrentPlan(currentPlan.id !== plan.id ? plan : null);
+    } else {
+      setCurrentPlan(plan);
+    }
+  };
+
   if (!plan) return null;
   return (
     <Container
       isActive={currentPlan && currentPlan.id === plan.id}
       color={plan.color}
-      onClick={(e) => setCurrentPlan(plan)}
+      onClick={(e) => handleClick(e)}
     >
       <Content color={plan.color}>
         <Title>{plan.title || ""}</Title>
